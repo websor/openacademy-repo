e0ab783f03fe63b06c5cc7b85633e65413b97855
Alfredo Morales <websor@stellarinc.org>
2021-01-21 13:15:26 -0800

    [ADD] openacademy: Use menu, xml files, basic fields

---
+++ openacademy/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "openacademy",
+
+    'summary': """Manage Training""",
+        
+    'description': """
+    Open Academy module for managin trainings
+    - Training COurses
+    - Training sessions
+    - Attendes Registration
+    """,
+
+    'author': "Alfredo E Morales P - Stellar Wholesale Inc",
+    'website': "http://www.stellarinc.ca",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/templates.xml',
+        'views/openacademy.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ openacademy/models/models.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+    _description = 'Model to store Courses'
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
