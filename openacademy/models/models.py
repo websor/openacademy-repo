496dde23c6f0d7ad02c31ebc622e2e9b97f575e6
Alfredo Morales <websor@stellarinc.org>
2021-01-22 14:41:03 -0800

    openacademy: ADDED fields: Many2Many and Many2one

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -8,6 +8,10 @@ class Course(models.Model):
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+    responsible_id = fields.Many2one(
+        'res.users', string="Responsible",
+        index="true", ondelete='set null'
+    )
 
 class Professor(models.Model):
     _name = 'openacademy.professor'
@@ -15,3 +19,15 @@ class Professor(models.Model):
 
     name = fields.Char(string="Name", required=True)
     description = fields.Text()
+
+class Session(models.Model):
+    _name = 'openacademy.session'
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6,2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+    instructor_id = fields.Many2one('res.partner', string='Instructor')
+    course_id = fields.Many2one('openacademy.course', ondelete='cascade', 
+                            string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner',string="Attendees")
