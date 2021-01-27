2db74a093a0d767255780dbb047ceca5813df78b
Alfredo Morales <websor@stellarinc.org>
2021-01-27 15:36:14 -0800

    Openacademy: ADDED Herencia class, new manifest, init model, partner.py and models.py, openacademy.xml, partner.xml

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -28,6 +28,7 @@
         'security/ir.model.access.csv',
         'views/templates.xml',
         'views/openacademy.xml',
+        'views/partner.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -7,7 +7,9 @@ class Course(models.Model):
     _description = 'Model to store Courses'
 
     name = fields.Char(string="Title", required=True)
+    
     description = fields.Text()
+
     responsible_id = fields.Many2one(
         'res.users', string="Responsible",
         index="true", ondelete='set null'
@@ -29,7 +31,7 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6,2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
-    instructor_id = fields.Many2one('res.partner', string='Instructor')
+    instructor_id = fields.Many2one('res.partner', string='Instructor', domain=[('instructor','=',True)])
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', 
                             string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner',string="Attendees")
