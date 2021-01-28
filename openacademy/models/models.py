45762871d823e03441038eab4e1b20e940157b72
Alfredo Morales <websor@stellarinc.org>
2021-01-27 16:20:47 -0800

    Openacademy: ADDED Campos y valores calculados, new models.py and openacademy.xml

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -29,9 +29,20 @@ class Session(models.Model):
 
     name = fields.Char(required=True)
     start_date = fields.Date()
+    datetime_test = fields.Datetime(default=fields.Datetime.now)
     duration = fields.Float(digits=(6,2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     instructor_id = fields.Many2one('res.partner', string='Instructor', domain=[('instructor','=',True)])
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', 
                             string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner',string="Attendees")
+    taken_seats = fields.Float(compute='_taken_seats')
+    active = fields.Boolean(default=True)
+
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        for record in self:
+            if not record.seats:
+                record.taken_seats = 0
+            else:
+                record.taken_seats = 100.0 * len(record.attendee_ids) / record.seats
